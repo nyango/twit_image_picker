7228d378d386334dfc4265dc2e9509c3bb1fe8b0
nyango <nyango@users.noreply.github.com>
2016-10-01 21:33:21 +0900

    Update to use GoogleCloudVisionAPI in order to pick adult images

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 twit_image_picker
 ----------------------
 
+It PICKS adult or medical or spoofs or violent images from Twitter using Google Cloud Vision API.
+
+I added to zshrc like
+```
+export GOOGLE_APPLICATION_CREDENTIALS=/PATH/TO/CREDENTIAL/FILE
+export GCLOUD_PROJECT=GOOGLE_API_ID
+```
+
 
 ## Install
 0. get and install `nodejs`, if you did not install it.
 1. `$ npm install`
 
-### Setup API Key
+### Setup Twitter API Key
 1. put your api keys(you can get them from https://apps.twitter.com/)
 2. write them in a file named `twit_params.js` as following
 
@@ -20,6 +28,12 @@ module.exports = {
 };
 ```
 
+### Setup Google Cloud Vision API
+1. setup according to https://cloud.google.com/vision/docs/quickstart
+
 ## How to use
-1. Install and Setup API Key, first.
-2. `$ node getImages.js` will show you image urls
+1. Install and setup as above
+2.
+```
+$ node getImages.js "WORD TO SEARCH" "num of tweets"
+```
--- getImages.js
+++ getImages.js
@@ -1,17 +1,61 @@
+if (process.argv.length < 3) {
+    console.log('missing argument.');
+    return;
+}
+
+var search_word = process.argv[2];
+var num_of_tweet = process.argv[3];
+
+// twitter auth
 var twit = require('twit');
 var twit_params = require('./twit_params.js');
 
+// google cloud api
+var Vision = require('@google-cloud/vision');
+var vision = Vision();
+
 var T = new twit(twit_params);
 
-T.get('search/tweets', { q: '描いてみた', count: 10 }, function(e,d,r) {
+var search_params = {
+    q: search_word,
+    count: parseInt(num_of_tweet),
+    include_entities: true,
+    locale: 'ja',
+    result_type: 'mixed'
+};
+
+var image_urls = [];
+
+var extract_image_urls = function(e,d,r) {
     d.statuses.map(function(st){
         if (st.extended_entities) {
             st.extended_entities.media.map(function(m){
-                console.log(m.media_url);
+                var url = m.media_url;
+                vision.detectSafeSearch(url, function (err, safeSearch, apiResponse) {
+                  if (safeSearch.adult) {
+                      console.log('adult!');
+                      console.log(url);
+                  }
+                  if (safeSearch.spoof) {
+                      console.log('spoof!');
+                      console.log(url);
+                  }
+                  if (safeSearch.medical) {
+                      console.log('medical!');
+                      console.log(url);
+                  }
+                  if (safeSearch.violence) {
+                      console.log('violence!');
+                      console.log(url);
+                  }
+                });
             });
         }
     });
-});
+};
+
+T.get('search/tweets', search_params, extract_image_urls);
+
 
 //
 //T.get('users/suggestions/:slug', { slug: 'funny'  }, function (err, data, response) {
